5c230ab81ea33ffad4ac7494b360d04e0f1ffb20
IlyaZolin <havvwale@gmail.com>
2022-11-21 10:09:21 +0300

    main

--- noupdateprod.php
+++ noupdateprod.php
@@ -2,23 +2,23 @@
 AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("MyClass", "CheckOwnerBeforeUpdate"));
 
 class MyClass {
-	public function CheckOwnerBeforeUpdate(&$arParams) {
-      	global $APPLICATION, $USER;
-      
-      	$rsElement = CIBlockElement::GetByID($arParams["ID"]);
-      	$arElement = $rsElement->GetNext();
-      
-		$date = new DateTime('-7 days');
-		$dateminus[] = $date->format('Y-m-d');
+public function CheckOwnerBeforeUpdate(&$arParams) {
+global $APPLICATION, $USER;
 
-		$dat = date_create($arElement['TIMESTAMP_X']);
-		$datecreate[] = date_format($dat, 'Y-m-d');
+$rsElement = CIBlockElement::GetByID($arParams["ID"]);
+$arElement = $rsElement->GetNext();
 
-		if ($dateminus['0'] > $datecreate['0']) {
-			/* 8 и больше */
-		}else{
-			$APPLICATION->ThrowException('Товар '. $arElement['NAME'] .' был создан менее одной недели назад и не может быть изменен.'); 
-			return false;
-		}
-	}
+$date = new DateTime('-7 days');
+$dateminus[] = $date->format('Y-m-d');
+
+$dat = date_create($arElement['TIMESTAMP_X']);
+$datecreate[] = date_format($dat, 'Y-m-d');
+
+if ($dateminus['0'] > $datecreate['0']) {
+/* 8 и больше */
+}else{
+$APPLICATION->ThrowException('Товар '. $arElement['NAME'] .' был создан менее одной недели назад и не может быть изменен.');
+return false;
+}
+}
 }
